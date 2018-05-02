8125b3b90ea738f16789f64132d771c79584b65d
rubiete <33655421+rubiete@users.noreply.github.com>
2018-05-02 15:11:29 +0200

    Update Esfera.java

--- Esfera.java
+++ Esfera.java
@@ -2,11 +2,13 @@ package geometria;
 
 public class Esfera {
 	private Segmento radio;
+	private Punto centro;
 	
-	public Esfera(Segmento radio) {
+	public Esfera(Segmento radio, Punto centro) {		
 		this.radio = radio;
+		this.centro = centro;
 	}
-	
+
 	public Segmento getRadio() {
 		return radio;
 	}
@@ -14,6 +16,15 @@ public class Esfera {
 	public void setRadio(Segmento radio) {
 		this.radio = radio;
 	}
+	
+	public Punto getCentro() {
+		return centro;
+	}
+
+	public void setCentro(Punto centro) {
+		this.centro = centro;
+	}
+
 	public double getArea(){
 		double area;
 		area=radio.getDistance()*radio.getDistance()*4*3.14159265359;
